eb9c4a3862fe55d1932c5c44ce353026e672aee3
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2024-01-21 10:27:36 +0530

    Create 3014.cpp

---
+++ 3014.cpp
@@ -0,0 +1,32 @@
+class Solution 
+{
+public:
+    int minimumPushes(string word) 
+    {
+        vector<int>f(26,0);
+        for(auto x:word)
+        {
+            f[x-'a']++;
+        }
+        sort(f.begin(),f.end());
+        int ans=0;
+        reverse(f.begin(),f.end());
+        for(int i=0;i<8;i++)
+        {
+            ans+=f[i];
+        }
+        for(int i=8;i<16;i++)
+        {
+            ans+=2*f[i];
+        }
+        for(int i=16;i<24;i++)
+        {
+            ans+=3*f[i];
+        }
+        for(int i=24;i<26;i++)
+        {
+            ans+=4*f[i];
+        }
+        return ans;
+    }
+};
