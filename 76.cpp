a4dd0c2dc101f240bee59b05c7424576b98e2e63
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2024-02-04 09:30:44 +0530

    Create 76.cpp

---
+++ 76.cpp
@@ -0,0 +1,51 @@
+bool check( map<char,int>&curr, map<char,int>&dp)
+{
+    for(auto x:dp)
+    {
+        if(curr[x.first]<x.second)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+class Solution 
+{
+public:
+    string minWindow(string s, string t) 
+    {
+        int n=s.size();
+        map<char,int>dp;
+        for(auto x:t)
+        {
+            //cout<<x<<endl;
+            dp[x]++;
+        }
+        int left=0;
+        int al=0;
+        int ar=-1;
+        map<char,int>curr;
+        for(int right=0;right<n;right++)
+        {
+            //cout<<right<<endl;
+            curr[s[right]]++;
+            
+            while(check(curr,dp) && left<=right)
+            {
+                if(ar==-1 || (ar-al+1)>(right-left+1))
+                {
+                    al=left;
+                    ar=right;
+                    
+                }
+                curr[s[left]]--;
+                left++;
+            }
+        }
+        if(ar==-1)
+        {
+            return "";
+        }
+        return s.substr(al,ar-al+1);
+    }
+};
