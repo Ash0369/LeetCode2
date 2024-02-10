21fccbe410c529e4d74b2125c4ef89ff5880bbfc
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2024-02-10 21:09:42 +0530

    Update 3026.cpp

--- 3026.cpp
+++ 3026.cpp
@@ -8,16 +8,11 @@ public:
         vector<ll int>dp(n,nums[0]);
         map<ll int,vector<int>>index;
         index[nums[0]].push_back(0);
+        ll int ans=-1e18;
         for(int i=1;i<n;i++)
         {
             dp[i]=dp[i-1]+nums[i];
-            index[nums[i]].push_back(i);
-        }
-        
-        ll int ans=-1e18;
-        
-        for(int i=0;i<n;i++)
-        {
+            
             ll int val=nums[i];
             ll int s=val+k;
             
@@ -35,6 +30,41 @@ public:
                 
                 ans=max(ans,s1-s2);
             }
+            s=val-k;
+            
+            for(auto x:index[s])
+            {
+                ll int a=min(i,x);
+                ll int b=max(i,x);
+                
+                ll int s1=dp[b];
+                ll int s2=0;
+                if(a>0)
+                {
+                    s2=dp[a-1];
+                }
+                
+                ans=max(ans,s1-s2);
+            }
+            
+            if(index[nums[i]].size()>0)
+            {
+                ll int ps=0;
+                if(index[nums[i]].back()-1>=0)
+                {
+                    ps=dp[index[nums[i]].back()-1];
+                }
+                if(ps>=dp[i-1])
+                {
+                    index[nums[i]].pop_back();
+                    index[nums[i]].push_back(i);
+                }
+            }
+            else
+            {
+                index[nums[i]].push_back(i);
+            }
+            
         }
         if(ans==(-1e18))
         {
