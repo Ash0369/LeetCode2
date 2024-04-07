4d31983de52e62907bbc8ed78a6885369342f4fb
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2024-04-07 13:32:23 +0530

    Create 3107.cpp

---
+++ 3107.cpp
@@ -0,0 +1,22 @@
+class Solution 
+{
+public:
+    long long minOperationsToMakeMedianK(vector<int>& nums, int k) 
+    {
+        #define ll long long
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+
+        ll int ans=0;
+
+        for(int i=0;i<=n/2;i++)
+        {
+            ans+=max(0,nums[i]-k);
+        }
+        for(int i=n/2;i<n;i++)
+        {
+            ans+=max(0,k-nums[i]);
+        }
+        return ans;
+    }
+};
